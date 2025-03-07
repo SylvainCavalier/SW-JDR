b241b8d74c304b23e3481dad65b71ed1dc1d68c7
Sylvain Bertrand <sylv.cavalier@gmail.com>
2025-03-07 20:31:04 +0100

    Correctif logo

--- app/models/apprentice.rb
+++ app/models/apprentice.rb
@@ -4,5 +4,8 @@ class Apprentice < ApplicationRecord
   has_many :pet_skills, through: :pet
   has_many :skills, through: :pet_skills
 
-  validates :name, presence: true
+  validates :jedi_name, presence: true
+  validates :side, numericality: { only_integer: true, greater_than_or_equal_to: -100, less_than_or_equal_to: 100 }
+  validates :speciality, inclusion: { in: ["Commun", "Gardien", "Consulaire", "Sentinelle"] }
+  validates :midi_chlorians, numericality: { only_integer: true, greater_than_or_equal_to: 5000, less_than_or_equal_to: 20000 }
 end
